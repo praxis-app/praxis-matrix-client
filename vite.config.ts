5b485809e60d963eb4d74a67f646227d3780ce6e
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-04-18 21:23:10 -0400

    chore: add prettier, remove vite init code

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import react from "@vitejs/plugin-react";
-import * as dotenv from "dotenv";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import * as dotenv from 'dotenv';
+import { defineConfig } from 'vite';
 
 dotenv.config();
 
 // https://vite.dev/config
 export default defineConfig({
   server: {
-    port: parseInt(process.env.PORT || "3000"),
+    port: parseInt(process.env.PORT || '3000'),
   },
   plugins: [react()],
 });
