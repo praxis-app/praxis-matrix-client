fb7e59bfc33f068c696f16bb32df44e4fcf978b2
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-04-20 14:14:43 -0400

    feat: set inter font for button

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from './components/ui/button';
 const App = () => {
   return (
     <div className="flex h-screen w-full items-center justify-center">
-      <Button className="">Praxis Matrix Client</Button>
+      <Button className="font-inter">Praxis Matrix Client</Button>
     </div>
   );
 };
