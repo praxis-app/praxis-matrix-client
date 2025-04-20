494307b90912ab7d4d0e0988f58f8d5e1af5ca65
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-04-20 13:20:51 -0400

    chore: add shadcn setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
+import { Button } from './components/ui/button';
+
 const App = () => {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        width: '100vw',
-        backgroundColor: 'black',
-        color: 'white',
-        fontSize: '2rem',
-      }}
-    >
-      Praxis Matrix Client
-    </div>
-  );
+  return <Button>Praxis Matrix Client</Button>;
 };
 
 export default App;
