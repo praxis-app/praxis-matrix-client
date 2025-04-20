edbb920b283915172467faf6bd532b44ab4adc37
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-04-20 14:19:27 -0400

    feat: set inter font globally

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from './components/ui/button';
 const App = () => {
   return (
     <div className="flex h-screen w-full items-center justify-center">
-      <Button className="font-inter">Praxis Matrix Client</Button>
+      <Button>Praxis Matrix Client</Button>
     </div>
   );
 };
