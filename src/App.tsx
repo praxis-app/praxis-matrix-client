fc87b82b5a4a2daf3a6f3a55103de2a2beac0dfc
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-04-20 18:17:20 -0400

    feat: add useMatrixClient hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { useEffect } from 'react';
 import LoginForm from './components/auth/login-form';
+import { useMatrixClient } from './hooks/shared.hooks';
 
 const App = () => {
+  const matrixClient = useMatrixClient();
+
+  useEffect(() => {
+    if (matrixClient) {
+      const init = async () => {
+        const { total_room_count_estimate } = await matrixClient.publicRooms();
+        console.log('Public room count:', total_room_count_estimate);
+      };
+      init();
+    }
+  }, [matrixClient]);
+
   return (
     <main className="flex flex-col items-center justify-center p-24">
-      <LoginForm />
+      {!matrixClient ? (
+        <>Loading...</>
+      ) : matrixClient.isLoggedIn() ? (
+        <>Logged in</>
+      ) : (
+        <LoginForm />
+      )}
     </main>
   );
 };
