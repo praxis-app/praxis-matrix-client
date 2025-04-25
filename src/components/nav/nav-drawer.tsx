5875a14f7f15e27beb0dfec23d0f66fcb655d26b
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-04-24 22:58:27 -0400

    feat: add room list container

--- src/components/nav/nav-drawer.tsx
+++ src/components/nav/nav-drawer.tsx
@@ -27,15 +27,20 @@ export const NavDrawer = ({ trigger }: Props) => {
           </Button>
         )}
       </SheetTrigger>
-      <SheetContent side="left" className="min-w-[100%] pt-4" hideCloseButton>
+      <SheetContent
+        side="left"
+        className="min-w-[100%] px-0 pt-4"
+        hideCloseButton
+      >
         <SheetHeader>
-          <SheetTitle className="flex items-center gap-2">
+          <SheetTitle className="flex items-center gap-2 px-6 pb-2 font-medium tracking-[0.02em]">
             <img src={appIconImg} alt={t('brand')} className="size-9" />
             {t('brand')}
-            <ChevronRight className="mt-0.5 size-4 stroke-(--color-neutral-400)" />
+            <ChevronRight className="mt-0.5 ml-0.5 size-4 stroke-(--color-zinc-400)" />
           </SheetTitle>
           <SheetDescription className="text-left"></SheetDescription>
         </SheetHeader>
+        <div className="h-full w-full rounded-t-2xl bg-(--accent) p-2"></div>
       </SheetContent>
     </Sheet>
   );
