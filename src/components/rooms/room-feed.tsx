d0ff6eacd81116b552789d6f82e9f34bd71e3a31
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-04-28 22:33:42 -0400

    feat: add room feed

---
+++ src/components/rooms/room-feed.tsx
@@ -0,0 +1,46 @@
+import { MatrixEvent, Room, RoomEvent } from 'matrix-js-sdk';
+import { Message } from '../messages/message';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useMatrixClient } from '@/hooks/use-matrix-client';
+
+interface Props {
+  room: Room;
+}
+
+export const RoomFeed = (props: Props) => {
+  const roomEvents = props.room.getLiveTimeline().getEvents();
+  const [messages, setMessages] = useState<MatrixEvent[]>(
+    roomEvents.filter((e) => e.getType() === 'm.room.message'),
+  );
+
+  const matrixClient = useMatrixClient();
+
+  useEffect(() => {
+    if (!matrixClient) {
+      return;
+    }
+
+    matrixClient.on(RoomEvent.Timeline, (event, room, toStart) => {
+      if (
+        event.getType() !== 'm.room.message' ||
+        props.room.roomId !== room?.roomId ||
+        toStart
+      ) {
+        return;
+      }
+      setMessages((prev) => {
+        const filtered = prev.filter((e) => e.getId() !== event.getId());
+        return [...filtered, event];
+      });
+    });
+  }, [matrixClient, props.room]);
+
+  return (
+    <div className="flex flex-1 flex-col p-2">
+      {messages.map((message) => (
+        <Message key={message.getId()} message={message} room={props.room} />
+      ))}
+    </div>
+  );
+};
--- src/components/rooms/room-view.tsx
+++ src/components/rooms/room-view.tsx
@@ -1,63 +1,25 @@
-import { useMatrixClient } from '@/hooks/use-matrix-client';
 import { useIsMobile } from '@/hooks/use-mobile';
-import { MatrixEvent, Room, RoomEvent } from 'matrix-js-sdk';
-import { useEffect, useState } from 'react';
-import { Message } from '../messages/message';
+import { Room } from 'matrix-js-sdk';
 import { MessageForm } from '../messages/message-form';
 import { LeftNav } from '../nav/left-nav';
+import { RoomFeed } from './room-feed';
 import { RoomTopNav } from './room-top-nav';
 
 interface Props {
   room: Room;
 }
 
-export const RoomView = (props: Props) => {
-  const roomEvents = props.room.getLiveTimeline().getEvents();
-  const [messages, setMessages] = useState<MatrixEvent[]>(
-    roomEvents.filter((e) => e.getType() === 'm.room.message'),
-  );
-
-  const matrixClient = useMatrixClient();
+export const RoomView = ({ room }: Props) => {
   const isMobile = useIsMobile();
-
-  useEffect(() => {
-    if (!matrixClient) {
-      return;
-    }
-
-    matrixClient.on(RoomEvent.Timeline, (event, room, toStart) => {
-      if (
-        event.getType() !== 'm.room.message' ||
-        props.room.roomId !== room?.roomId ||
-        toStart
-      ) {
-        return;
-      }
-      setMessages((prev) => {
-        const filtered = prev.filter((e) => e.getId() !== event.getId());
-        return [...filtered, event];
-      });
-    });
-  }, [matrixClient, props.room]);
 
   return (
     <div className="fixed top-0 right-0 bottom-0 left-0 flex">
       {!isMobile && <LeftNav />}
 
       <div className="flex flex-1 flex-col">
-        <RoomTopNav room={props.room} />
-
-        <div className="flex flex-1 flex-col p-2">
-          {messages.map((message) => (
-            <Message
-              key={message.getId()}
-              message={message}
-              room={props.room}
-            />
-          ))}
-        </div>
-
-        <MessageForm roomId={props.room.roomId} />
+        <RoomTopNav room={room} />
+        <RoomFeed room={room} />
+        <MessageForm roomId={room.roomId} />
       </div>
     </div>
   );
