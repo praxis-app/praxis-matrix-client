901fc2055ec46bf0b07f21d674b7c46260b12d5f
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-05-03 13:05:45 -0400

    feat: show welcome message

--- src/components/auth/auth-message.tsx
+++ src/components/auth/auth-message.tsx
@@ -1,11 +1,24 @@
 // TODO: Add welcome message
 
+import { useTranslation } from 'react-i18next';
 import { BotMessage } from '../messages/bot-message';
+import { Button } from '../ui/button/button';
 
 export const AuthMessage = () => {
+  const { t } = useTranslation();
+
   return (
     <BotMessage>
-      <div>Welcome to Praxis!</div>
+      <div className="pt-2 pb-2.5 text-[1.2rem]">
+        {t('prompts.welcomeToPraxis')}
+      </div>
+      <div className="pb-2.5">{t('welcome.messages.projectDescription1')}</div>
+      <div className="pb-2.5">{t('welcome.messages.projectDescription2')}</div>
+      <div className="pb-2.5">{t('welcome.messages.inDev')}</div>
+
+      <Button variant="secondary" className="mb-1 uppercase">
+        {t('auth.actions.logIn')}
+      </Button>
     </BotMessage>
   );
 };
