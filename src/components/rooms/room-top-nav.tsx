51101f798cef4c09ec2a4359e4fcd86535da1414
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-05-04 02:40:38 -0400

    feat: update background and foreground vars

--- src/components/rooms/room-top-nav.tsx
+++ src/components/rooms/room-top-nav.tsx
@@ -12,7 +12,7 @@ export const RoomTopNav = ({ room }: Props) => {
   const isMobile = useIsMobile();
 
   return (
-    <header className="flex h-[55px] items-center justify-between border-b border-[--color-border] bg-(--card) px-2.5">
+    <header className="bg-card flex h-[55px] items-center justify-between border-b border-[--color-border] px-2.5">
       <div className="flex flex-1 items-center">
         {isMobile && (
           <NavSheet
