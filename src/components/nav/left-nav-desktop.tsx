21597d06d8e502c8e278e9c51ebd4ce2469505fe
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-05-05 21:06:56 -0400

    feat: list rooms in left nav desktop

--- src/components/nav/left-nav-desktop.tsx
+++ src/components/nav/left-nav-desktop.tsx
@@ -1,8 +1,44 @@
+import { useMatrixClient } from '@/hooks/use-matrix-client';
+import { cn } from '@/lib/utils';
+import { Room } from 'matrix-js-sdk';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
 export const LeftNavDesktop = () => {
+  const [visibleRooms, setVisibleRooms] = useState<Room[]>([]);
+  const matrixClient = useMatrixClient();
+
+  const { roomId } = useParams();
+  const activeRoomId = roomId ?? visibleRooms[0]?.roomId;
+
+  useEffect(() => {
+    if (!matrixClient) {
+      return;
+    }
+    const init = async () => {
+      const rooms = matrixClient.getVisibleRooms();
+      setVisibleRooms(rooms);
+    };
+    init();
+  }, [matrixClient]);
+
   return (
     <div className="flex h-full w-[240px] flex-col border-r border-[--color-border] bg-(--card)">
       <div className="h-[55px] border-b border-[--color-border]"></div>
-      <div className="flex flex-1 flex-col"></div>
+      <div className="flex flex-1 flex-col overflow-y-scroll py-2">
+        {visibleRooms.map((room) => (
+          <Link
+            className={cn(
+              'text-muted-foreground hover:bg-accent mx-2 mb-0.5 rounded-[4px] px-2.5 py-0.5',
+              room.roomId === activeRoomId && 'bg-accent text-foreground',
+            )}
+            key={room.roomId}
+            to={`/rooms/${room.roomId}`}
+          >
+            <div>{room.name}</div>
+          </Link>
+        ))}
+      </div>
       <div className="h-[55px] border-t border-[--color-border]"></div>
     </div>
   );
