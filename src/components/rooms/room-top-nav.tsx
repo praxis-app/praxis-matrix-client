f28c7810ff953b07e75d866b08694b59cb52e354
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-05-06 20:09:37 -0400

    feat: add wip layout for room top nav

--- src/components/rooms/room-top-nav.tsx
+++ src/components/rooms/room-top-nav.tsx
@@ -1,6 +1,9 @@
 import { useIsMobile } from '@/hooks/use-mobile';
 import { Room } from 'matrix-js-sdk';
+import { useTranslation } from 'react-i18next';
 import { LuArrowLeft } from 'react-icons/lu';
+import { MdChevronRight, MdSearch } from 'react-icons/md';
+import { toast } from 'sonner';
 import { NavSheet } from '../nav/nav-sheet';
 import { Button } from '../ui/button';
 
@@ -9,22 +12,36 @@ interface Props {
 }
 
 export const RoomTopNav = ({ room }: Props) => {
+  const { t } = useTranslation();
   const isMobile = useIsMobile();
 
   return (
-    <header className="bg-card flex h-[55px] items-center justify-between border-b border-[--color-border] px-2.5">
-      <div className="flex flex-1 items-center">
+    <header className="bg-card flex h-[55px] items-center justify-between border-b border-[--color-border] px-2 md:pl-6">
+      <div className="flex flex-1 items-center gap-2.5">
         {isMobile && (
           <NavSheet
             trigger={
               <Button variant="ghost" size="icon">
-                <LuArrowLeft className="size-[1.45rem]" />
+                <LuArrowLeft className="size-6" />
               </Button>
             }
           />
         )}
-        <div className="flex flex-1 items-center">{room.name}</div>
+        <div className="flex flex-1 items-center text-[15px] font-medium">
+          {room.name}
+          {isMobile && (
+            <MdChevronRight className="text-muted-foreground mt-[0.07rem] size-5" />
+          )}
+        </div>
       </div>
+
+      <Button
+        onClick={() => toast(t('prompts.inDev'))}
+        variant="ghost"
+        size="icon"
+      >
+        <MdSearch className="size-6" />
+      </Button>
     </header>
   );
 };
