582720f809ccbe72e444f71d716b88793c93cd94
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-05-25 14:14:29 -0400

    feat: show name and description in room details drawer

--- src/components/rooms/room-top-nav.tsx
+++ src/components/rooms/room-top-nav.tsx
@@ -1,11 +1,20 @@
 import { useIsMobile } from '@/hooks/use-mobile';
-import { Room } from 'matrix-js-sdk';
+import { EventTimeline, Room } from 'matrix-js-sdk';
 import { useTranslation } from 'react-i18next';
 import { LuArrowLeft } from 'react-icons/lu';
 import { MdChevronRight, MdSearch } from 'react-icons/md';
 import { toast } from 'sonner';
 import { NavSheet } from '../nav/nav-sheet';
 import { Button } from '../ui/button';
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '../ui/drawer';
+import { Separator } from '../ui/separator';
 
 interface Props {
   room: Room;
@@ -15,9 +24,13 @@ export const RoomTopNav = ({ room }: Props) => {
   const { t } = useTranslation();
   const isMobile = useIsMobile();
 
+  const roomState = room.getLiveTimeline().getState(EventTimeline.FORWARDS);
+  const topicEvent = roomState?.getStateEvents('m.room.topic', '');
+  const topic = topicEvent ? topicEvent.getContent().topic : null;
+
   return (
     <header className="bg-card flex h-[55px] items-center justify-between border-b border-[--color-border] px-2 md:pl-6">
-      <div className="flex flex-1 items-center gap-2.5">
+      <div className="mr-1 flex flex-1 items-center gap-2.5">
         {isMobile && (
           <NavSheet
             trigger={
@@ -27,12 +40,28 @@ export const RoomTopNav = ({ room }: Props) => {
             }
           />
         )}
-        <div className="flex flex-1 items-center text-[15px] font-medium">
-          {room.name}
-          {isMobile && (
-            <MdChevronRight className="text-muted-foreground mt-[0.07rem] size-5" />
-          )}
-        </div>
+
+        <Drawer>
+          <DrawerTrigger asChild>
+            <div className="flex flex-1 items-center text-[15px] font-medium">
+              {room.name}
+              {isMobile && (
+                <MdChevronRight className="text-muted-foreground mt-[0.07rem] size-5" />
+              )}
+            </div>
+          </DrawerTrigger>
+
+          <DrawerContent className="flex min-h-[calc(100vh-55px)] flex-col items-start rounded-t-2xl border-0">
+            <DrawerHeader className="w-full pt-6 pb-7">
+              <DrawerTitle className="text-center text-[1.3rem]">
+                {room.name}
+              </DrawerTitle>
+              <DrawerDescription>{topic}</DrawerDescription>
+            </DrawerHeader>
+
+            <Separator />
+          </DrawerContent>
+        </Drawer>
       </div>
 
       <Button
