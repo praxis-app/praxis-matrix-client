fd6d0ae9f72e63d84408854c9660d5d245a005fe
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-05-26 01:04:44 -0400

    feat: add RoomSettingsForm

--- src/components/rooms/room-settings-sheet.tsx
+++ src/components/rooms/room-settings-sheet.tsx
@@ -1,34 +1,10 @@
 // TODO: Add i18n for all messages
 
-import { useMatrixClient } from '@/hooks/use-matrix-client';
-import { useRoomDirectoryVisibility } from '@/hooks/use-room-directory-visibility';
-import { t } from '@/lib/utils';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { EventTimeline, Room, Visibility } from 'matrix-js-sdk';
+import { Room } from 'matrix-js-sdk';
 import { ReactNode, useState } from 'react';
-import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { MdClose } from 'react-icons/md';
-import { toast } from 'sonner';
-import * as zod from 'zod';
 import { Button } from '../ui/button';
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '../ui/form';
-import { Input } from '../ui/input';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '../ui/select';
 import { Separator } from '../ui/separator';
 import {
   Sheet,
@@ -38,76 +14,17 @@ import {
   SheetTitle,
   SheetTrigger,
 } from '../ui/sheet';
-import { Textarea } from '../ui/textarea';
+import RoomSettingsForm from './room-settings-form';
 
 interface Props {
   trigger: ReactNode;
   room: Room;
 }
 
-const formSchema = zod.object({
-  name: zod
-    .string()
-    .min(3, {
-      message: t('rooms.errors.roomNameMin'),
-    })
-    .max(50, {
-      message: t('rooms.errors.roomNameMax'),
-    }),
-  description: zod
-    .string()
-    .max(500, {
-      message: t('rooms.errors.roomDescriptionMax'),
-    })
-    .optional(),
-  visibility: zod.enum(['public', 'private'], {
-    required_error: t('rooms.errors.roomVisibility'),
-  }),
-});
-
 const RoomSettingsSheet = ({ trigger, room }: Props) => {
-  const [isSubmitting, setIsSubmitting] = useState(false);
   const [open, setOpen] = useState(false);
 
-  const matrixClient = useMatrixClient();
   const { t } = useTranslation();
-
-  const roomState = room.getLiveTimeline().getState(EventTimeline.FORWARDS);
-  const topicEvent = roomState?.getStateEvents('m.room.topic', '');
-  const topic = topicEvent ? topicEvent.getContent().topic : null;
-
-  const visibility = useRoomDirectoryVisibility(room.roomId);
-
-  const form = useForm<zod.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    values: {
-      name: room.name,
-      description: topic,
-      visibility: visibility ?? Visibility.Private,
-    },
-  });
-
-  const handleSubmit = async (values: zod.infer<typeof formSchema>) => {
-    if (!matrixClient) {
-      return;
-    }
-    setIsSubmitting(true);
-
-    try {
-      // TODO: Implement room update
-      console.log(values);
-
-      toast(t('rooms.toasts.roomUpdated'));
-
-      form.reset();
-    } catch (error) {
-      toast(t('rooms.toasts.roomUpdatedError'), {
-        description: t('rooms.toasts.roomUpdatedErrorDescription'),
-      });
-    } finally {
-      setIsSubmitting(false);
-    }
-  };
 
   return (
     <Sheet open={open} onOpenChange={setOpen}>
@@ -133,92 +50,7 @@ const RoomSettingsSheet = ({ trigger, room }: Props) => {
 
         <Separator className="mb-7" />
 
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(handleSubmit)}
-            className="space-y-6 px-4"
-          >
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{t('rooms.labels.name')}</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder={t('rooms.placeholders.name')}
-                      autoComplete="off"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormDescription>
-                    {t('rooms.descriptions.roomName')}
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{t('rooms.labels.description')}</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      placeholder={t('rooms.placeholders.description')}
-                      className="resize-none"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormDescription>
-                    {t('rooms.prompts.roomDescription')}
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="visibility"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{t('rooms.labels.visibility')}</FormLabel>
-                  <Select
-                    onValueChange={field.onChange}
-                    defaultValue={field.value}
-                  >
-                    <FormControl>
-                      <SelectTrigger className="w-full">
-                        <SelectValue
-                          placeholder={t('rooms.placeholders.visibility')}
-                        />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      <SelectItem value={Visibility.Public}>
-                        {t('rooms.options.public')}
-                      </SelectItem>
-                      <SelectItem value={Visibility.Private}>
-                        {t('rooms.options.private')}
-                      </SelectItem>
-                    </SelectContent>
-                  </Select>
-                  <FormDescription>
-                    {t('rooms.descriptions.roomVisibility')}
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <Button type="submit" className="w-full" disabled={isSubmitting}>
-              {t('actions.save')}
-            </Button>
-          </form>
-        </Form>
+        <RoomSettingsForm room={room} />
       </SheetContent>
     </Sheet>
   );
