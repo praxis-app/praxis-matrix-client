75ece42ab5405be9c7f35ff568329efba00242cf
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-05-26 10:33:12 -0400

    feat: update room settings on mobile

--- src/components/rooms/room-settings-form.tsx
+++ src/components/rooms/room-settings-form.tsx
@@ -2,7 +2,13 @@ import { useMatrixClient } from '@/hooks/use-matrix-client';
 import { useRoomDirectoryVisibility } from '@/hooks/use-room-directory-visibility';
 import { t } from '@/lib/utils';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { EventTimeline, Room, Visibility } from 'matrix-js-sdk';
+import {
+  EventTimeline,
+  EventType,
+  Method,
+  Room,
+  Visibility,
+} from 'matrix-js-sdk';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
@@ -43,9 +49,11 @@ const formSchema = zod.object({
       message: t('rooms.errors.roomDescriptionMax'),
     })
     .optional(),
-  visibility: zod.enum(['public', 'private'], {
-    required_error: t('rooms.errors.roomVisibility'),
-  }),
+  visibility: zod
+    .enum(['public', 'private'], {
+      required_error: t('rooms.errors.roomVisibility'),
+    })
+    .optional(),
 });
 
 interface Props {
@@ -54,22 +62,30 @@ interface Props {
 
 const RoomSettingsForm = ({ room }: Props) => {
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isVisibilityLoading, setIsVisibilityLoading] = useState(true);
 
   const matrixClient = useMatrixClient();
   const { t } = useTranslation();
 
   const roomState = room.getLiveTimeline().getState(EventTimeline.FORWARDS);
-  const topicEvent = roomState?.getStateEvents('m.room.topic', '');
-  const topic = topicEvent ? topicEvent.getContent().topic : null;
-
-  const visibility = useRoomDirectoryVisibility(room.roomId);
+  const topicEvent = roomState?.getStateEvents(EventType.RoomTopic, '');
+  const topic: string | undefined = topicEvent
+    ? topicEvent.getContent().topic
+    : undefined;
 
   const form = useForm<zod.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    values: {
+    defaultValues: {
       name: room.name,
       description: topic,
-      visibility: visibility ?? Visibility.Private,
+    },
+  });
+
+  const visibility = useRoomDirectoryVisibility({
+    roomId: room.roomId,
+    onSuccess: (visibility) => {
+      form.setValue('visibility', visibility);
+      setIsVisibilityLoading(false);
     },
   });
 
@@ -80,13 +96,38 @@ const RoomSettingsForm = ({ room }: Props) => {
     setIsSubmitting(true);
 
     try {
-      // TODO: Implement room update
-      console.log(values);
+      if (values.name !== room.name) {
+        await matrixClient.sendStateEvent(
+          room.roomId,
+          EventType.RoomName,
+          {
+            name: values.name,
+          },
+          '',
+        );
+      }
+      const currentTopic = topic || '';
+      const newTopic = values.description || '';
+      if (newTopic !== currentTopic) {
+        await matrixClient.sendStateEvent(
+          room.roomId,
+          EventType.RoomTopic,
+          {
+            topic: newTopic,
+          },
+          '',
+        );
+      }
+      if (values.visibility !== visibility) {
+        const url = `/directory/list/room/${encodeURIComponent(room.roomId)}`;
+        await matrixClient.http.authedRequest(Method.Put, url, undefined, {
+          visibility: values.visibility,
+        });
+      }
 
       toast(t('rooms.toasts.roomUpdated'));
-
-      form.reset();
     } catch (error) {
+      console.error('Error updating room:', error);
       toast(t('rooms.toasts.roomUpdatedError'), {
         description: t('rooms.toasts.roomUpdatedErrorDescription'),
       });
@@ -94,6 +135,10 @@ const RoomSettingsForm = ({ room }: Props) => {
       setIsSubmitting(false);
     }
   };
+
+  if (isVisibilityLoading) {
+    return null;
+  }
 
   return (
     <Form {...form}>
--- src/hooks/use-room-directory-visibility.ts
+++ src/hooks/use-room-directory-visibility.ts
@@ -2,18 +2,32 @@ import { useEffect, useState } from 'react';
 import { useMatrixClient } from './use-matrix-client';
 import { Visibility } from 'matrix-js-sdk';
 
-export const useRoomDirectoryVisibility = (roomId: string) => {
+interface UseRoomDirectoryVisibilityProps {
+  roomId: string;
+  onSuccess?(visibility: Visibility | undefined): void;
+}
+
+// TODO: Decide whether this is needed
+export const useRoomDirectoryVisibility = ({
+  roomId,
+  onSuccess,
+}: UseRoomDirectoryVisibilityProps) => {
   const [visibility, setVisibility] = useState<Visibility>();
   const matrixClient = useMatrixClient();
 
   useEffect(() => {
+    if (!roomId || visibility) {
+      return;
+    }
+
     const init = async () => {
       const { visibility } =
         await matrixClient.getRoomDirectoryVisibility(roomId);
       setVisibility(visibility);
+      onSuccess?.(visibility);
     };
     init();
-  }, []);
+  }, [roomId, matrixClient, onSuccess]);
 
   return visibility;
 };
