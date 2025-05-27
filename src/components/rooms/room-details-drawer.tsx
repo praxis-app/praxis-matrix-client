70b448d78767a77a0a7b0fef83e7a66ac3fc054b
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-05-26 21:54:34 -0400

    feat: add wip leave room dialog

--- src/components/rooms/room-details-drawer.tsx
+++ src/components/rooms/room-details-drawer.tsx
@@ -1,8 +1,18 @@
 import { EventTimeline, Room } from 'matrix-js-sdk';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { BiDoorOpen } from 'react-icons/bi';
 import { MdChevronRight, MdSettings } from 'react-icons/md';
 import { Button } from '../ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '../ui/dialog';
 import {
   Drawer,
   DrawerContent,
@@ -20,6 +30,7 @@ interface Props {
 }
 
 export const RoomDetailsDrawer = ({ room, trigger }: Props) => {
+  const [showLeaveRoomDialog, setShowLeaveRoomDialog] = useState(false);
   const { t } = useTranslation();
 
   const roomState = room.getLiveTimeline().getState(EventTimeline.FORWARDS);
@@ -57,6 +68,47 @@ export const RoomDetailsDrawer = ({ room, trigger }: Props) => {
           }
           room={room}
         />
+
+        <Dialog
+          open={showLeaveRoomDialog}
+          onOpenChange={setShowLeaveRoomDialog}
+        >
+          <DialogTrigger asChild>
+            <Button
+              className="text-destructive mx-auto mt-6 h-[3.2rem] w-[92%] justify-start gap-3"
+              variant="secondary"
+              size="lg"
+            >
+              <BiDoorOpen className="size-6.5" />
+              {t('rooms.actions.leaveRoom')}
+            </Button>
+          </DialogTrigger>
+
+          <DialogContent>
+            <DialogHeader className="pb-0.5">
+              <DialogTitle className="text-center">
+                {t('rooms.headers.leaveRoom')}
+              </DialogTitle>
+            </DialogHeader>
+
+            <DialogDescription className="pb-2">
+              {t('rooms.prompts.leaveRoom')}
+            </DialogDescription>
+
+            <DialogFooter className="flex w-full flex-row gap-2 self-center">
+              <Button
+                variant="outline"
+                onClick={() => setShowLeaveRoomDialog(false)}
+                className="flex-1"
+              >
+                {t('actions.cancel')}
+              </Button>
+              <Button variant="destructive" className="flex-1">
+                {t('rooms.actions.leave')}
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </DrawerContent>
     </Drawer>
   );
