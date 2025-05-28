4993fd7edbdb3ff9c21deec194a953108985ac4d
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-05-28 17:44:27 -0400

    feat: leave rooms on desktop

--- src/components/rooms/room-list-item.tsx
+++ src/components/rooms/room-list-item.tsx
@@ -1,11 +1,17 @@
 import { cn } from '@/lib/utils';
 import { Room } from 'matrix-js-sdk';
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { MdSettings } from 'react-icons/md';
 import { Link, useNavigate } from 'react-router-dom';
-import { ContextMenuContent, ContextMenuItem } from '../ui/context-menu';
-import { ContextMenu, ContextMenuTrigger } from '../ui/context-menu';
-import { useTranslation } from 'react-i18next';
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from '../ui/context-menu';
+import { Dialog, DialogTrigger } from '../ui/dialog';
+import { LeaveRoomDialogContent } from './leave-room-dialog-content';
 
 interface Props {
   activeRoomId: string;
@@ -13,6 +19,7 @@ interface Props {
 }
 
 const RoomListItem = ({ activeRoomId, room }: Props) => {
+  const [showLeaveRoomDialog, setShowLeaveRoomDialog] = useState(false);
   const [isHovering, setIsHovering] = useState(false);
 
   const { t } = useTranslation();
@@ -25,39 +32,52 @@ const RoomListItem = ({ activeRoomId, room }: Props) => {
   const settingsPath = `${roomPath}/settings`;
 
   return (
-    <ContextMenu>
-      <ContextMenuTrigger>
-        <div
-          className={cn(
-            'text-muted-foreground hover:bg-accent mx-2 mb-0.5 flex items-center justify-between rounded-[4px] px-2.5 py-0.5',
-            isActive && 'bg-accent text-foreground',
-          )}
-          key={room.roomId}
-          onMouseEnter={() => setIsHovering(true)}
-          onMouseLeave={() => setIsHovering(false)}
-        >
-          <Link to={roomPath} className="mr-1.5 truncate">
-            {room.name}
-          </Link>
-          {showSettingsBtn && (
-            <Link to={settingsPath}>
-              <MdSettings
-                className={cn(
-                  'hover:text-foreground text-muted-foreground size-4.5',
-                  isActive && 'text-foreground',
-                )}
-              />
+    <Dialog open={showLeaveRoomDialog} onOpenChange={setShowLeaveRoomDialog}>
+      <ContextMenu modal={false}>
+        <ContextMenuTrigger>
+          <div
+            className={cn(
+              'text-muted-foreground hover:bg-accent mx-2 mb-0.5 flex items-center justify-between rounded-[4px] px-2.5 py-0.5',
+              isActive && 'bg-accent text-foreground',
+            )}
+            key={room.roomId}
+            onMouseEnter={() => setIsHovering(true)}
+            onMouseLeave={() => setIsHovering(false)}
+          >
+            <Link to={roomPath} className="mr-1.5 truncate">
+              {room.name}
             </Link>
-          )}
-        </div>
-      </ContextMenuTrigger>
+            {showSettingsBtn && (
+              <Link to={settingsPath}>
+                <MdSettings
+                  className={cn(
+                    'hover:text-foreground text-muted-foreground size-4.5',
+                    isActive && 'text-foreground',
+                  )}
+                />
+              </Link>
+            )}
+          </div>
+        </ContextMenuTrigger>
 
-      <ContextMenuContent>
-        <ContextMenuItem onClick={() => navigate(settingsPath)}>
-          {t('rooms.labels.settings')}
-        </ContextMenuItem>
-      </ContextMenuContent>
-    </ContextMenu>
+        <ContextMenuContent>
+          <ContextMenuItem onClick={() => navigate(settingsPath)}>
+            {t('rooms.labels.settings')}
+          </ContextMenuItem>
+
+          <DialogTrigger asChild>
+            <ContextMenuItem className="text-destructive">
+              {t('rooms.actions.leaveRoom')}
+            </ContextMenuItem>
+          </DialogTrigger>
+        </ContextMenuContent>
+      </ContextMenu>
+
+      <LeaveRoomDialogContent
+        setShowLeaveRoomDialog={setShowLeaveRoomDialog}
+        room={room}
+      />
+    </Dialog>
   );
 };
 
