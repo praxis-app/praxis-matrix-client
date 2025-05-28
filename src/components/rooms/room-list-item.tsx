e1416d2b4a8452298ad04e5fc26ff062ec2ecda9
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-05-28 17:23:55 -0400

    feat: add context menu for room list item with link to settings

--- src/components/rooms/room-list-item.tsx
+++ src/components/rooms/room-list-item.tsx
@@ -2,7 +2,10 @@ import { cn } from '@/lib/utils';
 import { Room } from 'matrix-js-sdk';
 import { useState } from 'react';
 import { MdSettings } from 'react-icons/md';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { ContextMenuContent, ContextMenuItem } from '../ui/context-menu';
+import { ContextMenu, ContextMenuTrigger } from '../ui/context-menu';
+import { useTranslation } from 'react-i18next';
 
 interface Props {
   activeRoomId: string;
@@ -12,34 +15,49 @@ interface Props {
 const RoomListItem = ({ activeRoomId, room }: Props) => {
   const [isHovering, setIsHovering] = useState(false);
 
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
   const isActive = room.roomId === activeRoomId;
   const showSettingsBtn = isHovering || isActive;
+
   const roomPath = `/rooms/${room.roomId}`;
+  const settingsPath = `${roomPath}/settings`;
 
   return (
-    <div
-      className={cn(
-        'text-muted-foreground hover:bg-accent mx-2 mb-0.5 flex items-center justify-between rounded-[4px] px-2.5 py-0.5',
-        isActive && 'bg-accent text-foreground',
-      )}
-      key={room.roomId}
-      onMouseEnter={() => setIsHovering(true)}
-      onMouseLeave={() => setIsHovering(false)}
-    >
-      <Link to={roomPath} className="mr-1.5 truncate">
-        {room.name}
-      </Link>
-      {showSettingsBtn && (
-        <Link to={`${roomPath}/settings`}>
-          <MdSettings
-            className={cn(
-              'hover:text-foreground text-muted-foreground size-4.5',
-              isActive && 'text-foreground',
-            )}
-          />
-        </Link>
-      )}
-    </div>
+    <ContextMenu>
+      <ContextMenuTrigger>
+        <div
+          className={cn(
+            'text-muted-foreground hover:bg-accent mx-2 mb-0.5 flex items-center justify-between rounded-[4px] px-2.5 py-0.5',
+            isActive && 'bg-accent text-foreground',
+          )}
+          key={room.roomId}
+          onMouseEnter={() => setIsHovering(true)}
+          onMouseLeave={() => setIsHovering(false)}
+        >
+          <Link to={roomPath} className="mr-1.5 truncate">
+            {room.name}
+          </Link>
+          {showSettingsBtn && (
+            <Link to={settingsPath}>
+              <MdSettings
+                className={cn(
+                  'hover:text-foreground text-muted-foreground size-4.5',
+                  isActive && 'text-foreground',
+                )}
+              />
+            </Link>
+          )}
+        </div>
+      </ContextMenuTrigger>
+
+      <ContextMenuContent>
+        <ContextMenuItem onClick={() => navigate(settingsPath)}>
+          {t('rooms.labels.settings')}
+        </ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
   );
 };
 
--- src/components/ui/context-menu.tsx
+++ src/components/ui/context-menu.tsx
@@ -75,7 +75,7 @@ const ContextMenuItem = React.forwardRef<
   <ContextMenuPrimitive.Item
     ref={ref}
     className={cn(
-      'focus:bg-accent focus:text-accent-foreground relative flex cursor-default items-center rounded-sm px-2 py-1.5 text-sm outline-none select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+      'focus:bg-accent focus:text-accent-foreground relative flex cursor-pointer items-center rounded-sm px-2 py-1.5 text-sm outline-none select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
       inset && 'pl-8',
       className,
     )}
