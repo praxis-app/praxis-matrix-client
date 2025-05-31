63ea99d6eb9a1aa46703739ab7c726b2f05a815a
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-05-31 19:43:35 -0400

    feat: sort joined rooms by the timestamp of the last event

--- src/hooks/use-joined-rooms.ts
+++ src/hooks/use-joined-rooms.ts
@@ -2,6 +2,16 @@ import { Room } from 'matrix-js-sdk';
 import { useEffect, useState } from 'react';
 import { useMatrixClient } from './use-matrix-client';
 
+const getLastEventTs = (room: Room) => {
+  const events = room.getLiveTimeline().getEvents();
+  return events[events.length - 1].getTs();
+};
+
+const sortRoomsByLastEventTs = (rooms: Room[]) =>
+  rooms.sort((a, b) => {
+    return getLastEventTs(b) - getLastEventTs(a);
+  });
+
 export const useJoinedRooms = () => {
   const [joinedRooms, setJoinedRooms] = useState<Room[]>([]);
   const matrixClient = useMatrixClient();
@@ -14,7 +24,8 @@ export const useJoinedRooms = () => {
       const filteredRooms = visibleRooms.filter((room) =>
         joined_rooms.includes(room.roomId),
       );
-      setJoinedRooms(filteredRooms);
+      const sortedRooms = sortRoomsByLastEventTs(filteredRooms);
+      setJoinedRooms(sortedRooms);
     };
     syncRooms();
   }, [matrixClient, visibleRooms.length]);
