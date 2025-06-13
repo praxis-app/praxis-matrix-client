d96aeb80d8c793574c0c70ac6e307e6dc0be8fbe
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-06-13 15:08:36 -0400

    feat: update message form to include plus button for menu

--- src/components/messages/message-form.tsx
+++ src/components/messages/message-form.tsx
@@ -8,12 +8,13 @@ import { MsgType } from 'matrix-js-sdk';
 import { KeyboardEventHandler, useEffect, useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
-import { MdImage, MdSend } from 'react-icons/md';
+import { BiSolidSend } from 'react-icons/bi';
 import { toast } from 'sonner';
 import * as zod from 'zod';
 import { Button } from '../ui/button';
 import { Form, FormField } from '../ui/form';
 import { Textarea } from '../ui/textarea';
+import { MdAdd, MdImage } from 'react-icons/md';
 
 const MESSAGE_BODY_MAX = 6000;
 
@@ -98,9 +99,17 @@ export const MessageForm = ({ roomId }: Props) => {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="bg-card w-full overflow-y-auto border-t p-2 pb-4"
+        className="flex w-full items-center gap-2 overflow-y-auto border-t p-2 pt-2.5 pb-4"
       >
-        <div className="bg-input/30 rounded-2xl p-1 transition-colors duration-200">
+        <Button
+          type="submit"
+          className="bg-input/30 size-11 rounded-full"
+          disabled={form.formState.isSubmitting}
+        >
+          <MdAdd className="text-muted-foreground size-7" />
+        </Button>
+
+        <div className="bg-input/30 flex w-full items-center rounded-3xl px-2">
           <FormField
             control={form.control}
             name="body"
@@ -108,7 +117,7 @@ export const MessageForm = ({ roomId }: Props) => {
               <Textarea
                 {...field}
                 placeholder={t('messages.placeholders.sendMessage')}
-                className="min-h-12 resize-none border-none bg-transparent shadow-none focus-visible:border-none focus-visible:ring-0 dark:bg-transparent"
+                className="min-h-12 resize-none border-none bg-transparent py-3 shadow-none focus-visible:border-none focus-visible:ring-0 dark:bg-transparent"
                 onKeyDown={handleInputKeyDown}
                 ref={inputRef}
                 rows={1}
@@ -116,26 +125,24 @@ export const MessageForm = ({ roomId }: Props) => {
             )}
           />
 
-          <div className="flex justify-between">
-            <Button
-              variant="ghost"
-              size="icon"
-              className="rounded-full"
-              disabled={form.formState.isSubmitting}
-            >
-              <MdImage className="text-muted-foreground size-6" />
-            </Button>
-            <Button
-              type="submit"
-              variant="ghost"
-              size="icon"
-              className="rounded-full"
-              disabled={form.formState.isSubmitting}
-            >
-              <MdSend className="text-muted-foreground size-5" />
-            </Button>
-          </div>
+          <Button
+            type="submit"
+            size="icon"
+            className="rounded-full"
+            variant="ghost"
+            disabled={form.formState.isSubmitting}
+          >
+            <MdImage className="text-muted-foreground size-6" />
+          </Button>
         </div>
+
+        <Button
+          type="submit"
+          className="size-10 rounded-full"
+          disabled={form.formState.isSubmitting}
+        >
+          <BiSolidSend className="ml-0.5 size-5" />
+        </Button>
       </form>
     </Form>
   );
