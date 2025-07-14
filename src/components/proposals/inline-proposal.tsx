e085d4399341ed24607031c8aebb86198a5db12e
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-07-13 21:29:39 -0400

    feat: add left border to better distinguish inline proposals

--- src/components/proposals/inline-proposal.tsx
+++ src/components/proposals/inline-proposal.tsx
@@ -41,7 +41,7 @@ export const InlineProposal = ({ proposal, room }: Props) => {
           <div className="text-muted-foreground text-sm">{formattedDate}</div>
         </div>
 
-        <Card className="w-full gap-3.5 rounded-md px-3 py-3.5">
+        <Card className="before:border-l-border relative w-full gap-3.5 rounded-md px-3 py-3.5 before:absolute before:inset-0 before:mt-[-0.025rem] before:mb-[-0.025rem] before:rounded-md before:border-l-3">
           <div className="flex items-center gap-1.5 font-medium">
             <FaClipboard className="mb-0.5" />
             {t('proposals.labels.consensusProposal')}
